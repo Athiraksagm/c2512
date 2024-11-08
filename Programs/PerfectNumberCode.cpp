a542b941b4ef729390e99353441215766568fd02
Athira K S <athiraksagm@gmail.com>
2024-11-08 14:23:58 +0530

    Update PerfectNumberCode.cpp

--- Programs/PerfectNumberCode.cpp
+++ Programs/PerfectNumberCode.cpp
@@ -1,33 +1,48 @@
 #include<iostream>
-using namespace std;
+using std::cout;
+using std::cout;
+
 bool perfectNumber(int number)
 {
  int divisor;
- int sum=0;
- for(int i=1;i<number;i++)
+ int sum = 0;
+ 
+ for(int i=1;i< number;i++)
  {
-    divisor=number%i;
-    if(divisor==0)
-      sum=sum+i;
+    divisor = number % i;
+    if(divisor == 0)
+      sum = sum + i;
  }
-if(sum==number)
+if(sum == number)
+{
     return true;
+}
 else
+{
     return false;
 }
-void perfectNumberTest()
+}
+
+void perfectNumberTest(void)
 {
  int number;
- bool result=false;
- cout<<" Enter the number to be checked : ";
- cin>>number;
- result=perfectNumber(number);
+ bool result = false;
+ 
+ cout << " Enter the number to be checked : ";
+ cin >> number;
+ 
+ result = perfectNumber(number);
  if(result)
-      cout<<number<< "is a perfect number ";
+ {
+      cout << number << "is a perfect number ";
+ }
  else
-      cout<<number<< "is not a perfect number ";
+ {
+      cout << number << "is not a perfect number ";
+ }
 }
-int main()
+
+int main(void)
 {
  perfectNumberTest();
  return 0;
