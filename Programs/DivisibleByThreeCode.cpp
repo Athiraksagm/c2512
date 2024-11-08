734f61e73ded9bdb368bfbd94ce2c44200c0cdc5
Athira K S <athiraksagm@gmail.com>
2024-11-08 14:16:06 +0530

    Update DivisibleByThreeCode.cpp

--- Programs/DivisibleByThreeCode.cpp
+++ Programs/DivisibleByThreeCode.cpp
@@ -1,24 +1,41 @@
 #include<iostream>
-using namespace std;
-bool divisibleByThree(int number){
+using std::cout;
+using std::cin;
+
+bool divisibleByThree(int number)
+{
   int number;
-  if(number%3==0)
+  if(number % 3 == 0)
+  {
      return true;
+  }
   else 
+  {
       return false;
+  }
 }
-void divisibleByThreeTest(){
+
+void divisibleByThreeTest(void)
+{
     int number;
     bool result=false;
-    cout<<"Enter a Number to be checked : ";
-    cin>>number;
-    result=divisibleByThree(number);
+  
+    cout << "Enter a Number to be checked : ";
+    cin >> number;
+  
+    result = divisibleByThree(number);
     if(result)
-        cout<<number<< " is divisible by three";
+    {
+        cout << number << " is divisible by three";
+    }
     else
-        cout<<number<< " is not divisible by three";
+    {
+        cout << number << " is not divisible by three";
+    }
 }
-int main(){
+
+int main(void)
+{
      divisibleByThreeTest();
      return 0;
 }
