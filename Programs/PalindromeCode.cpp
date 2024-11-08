7b3bda8130b778013e07a178ad14751b7367de90
Athira K S <athiraksagm@gmail.com>
2024-11-08 14:19:51 +0530

    Update PalindromeCode.cpp

--- Programs/PalindromeCode.cpp
+++ Programs/PalindromeCode.cpp
@@ -1,35 +1,54 @@
 #include<iostream>
 #include<string>
-using namespace std;
-bool palindromeOrNot(string str){
+using std::cout;
+using std::cin;
+
+bool palindromeOrNot(string str)
+{
   string str;
   string temp;
-  int i=0,j;
-  temp=str;
-  j=str.length()-1;
+  int i = 0, j;
+  temp = str;
+  j = str.length()-1;
+  
   while(i<j)
    {
-      swap(str[i],str[j]);
+      swap(str[i], str[j]);
       i++;
       j--;
    }
-  if(temp==str)
+  
+  if(temp == str)
+  {
       return true;
+  }
   else 
+  {
       return false;
+  }
 }
-void palindromeOrNotTest(){
+
+void palindromeOrNotTest(void)
+{
     string str;
-    bool result=false;
-    cout<<"Enter a string to be checked : ";
-    cin>>str;
-    result=palindromeOrNot(str);
+    bool result = false;
+  
+    cout << "Enter a string to be checked : ";
+    cin >> str;
+  
+    result = palindromeOrNot(str);
     if(result)
+    {
         cout<<str<< " is a Palindrome ";
+    }
     else
-        cout<<str<< " is not a Palindrome ";
+    {
+        cout << str << " is not a Palindrome ";
+    }
 }
-int main(){
+
+int main()
+{
      palindromeOrNotTest();
      return 0;
 }
