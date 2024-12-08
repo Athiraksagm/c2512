5b20eb9d22625e0431a0e75e7a3aa289ff3b6fae
Athira K S <athiraksagm@gmail.com>
2024-12-08 12:42:48 +0530

    Create Divisible.cpp

---
+++ Phase 1/Day4/Divisible.cpp
@@ -0,0 +1,6 @@
+Write a program that continuously prompts the user to enter a number. 
+The program should add the entered number to the previous valid number if it is divisible by 3 or 5 and display the sum. 
+If the number is not divisible by 3 or 5, display a message indicating this. The program should terminate when the user types any particular character. 
+Additionally, handle invalid input by prompting the user to enter a valid number.
+
+[both pseudo code and C++ code]
