1a32dcca6529b68d83c95886f91f98a71bab986e
Athira K S <athiraksagm@gmail.com>
2024-12-08 21:50:06 +0530

    Create Task2.cpp

---
+++ Phase 1/Day23/Task2.cpp
@@ -0,0 +1,13 @@
+single inheritance using static binding:
+=============================================================
+Create two static objects for Both Base and Derived class
+and call all behaviours
+and 
+one two dynamic objects for Base and Derived class 
+and call all behaviours
+
+Define constructors and destructors for all the classes 
+
+Problem 15: `Sports Management`
+- Create a base class `Player` with fields `name` and `age`, and a behavior `train()`.
+- Derive a class `CricketPlayer` with fields `battingAverage` and a behavior `playMatch()`.
