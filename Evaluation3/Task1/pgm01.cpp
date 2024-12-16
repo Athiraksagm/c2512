9365fcca422e9437606e273fcf23141531e56659
Athira K S <athiraksagm@gmail.com>
2024-12-16 23:14:35 +0530

    Update pgm01.cpp

--- Evaluation3/Task1/pgm01.cpp
+++ Evaluation3/Task1/pgm01.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee {
+private:
+    int id;
+    int age;
+    string name;
+
+public:
+    // Constructor
+    Employee(int id, int age, const string& name) : id(id), age(age), name(name) {}
+
+    // Friend function to overload the << operator
+    friend ostream& operator<<(ostream& os, const Employee& emp) {
+        output << emp.id << " " << emp.age << " " << emp.name;
+        return output;
+    }
+
+    // Swap method
+    void swap(Employee& other) {
+        std::swap(this->id, other.id);
+        std::swap(this->age, other.age);
+        std::swap(this->name, other.name);
+    }
+};
+
+int main() {
+    Employee e1(101, 22, "Athira");
+    Employee e2(102, 23, "Bhagya");
+
+    cout << e1 << endl; // 101 22 Athira
+    cout << e2 << endl; // 102 23 Bhagya
+
+    e1.swap(e2);
+
+    cout << e1 << endl; // 102 23 Bhagya
+    cout << e2 << endl; // 101 22 Athira
+
+    return 0;
+}
 
