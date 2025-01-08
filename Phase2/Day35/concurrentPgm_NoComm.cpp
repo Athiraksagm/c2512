41d22a45b7b7b63ca7d48083cb5bbaa3a5b7ac1c
Athira K S <athiraksagm@gmail.com>
2025-01-08 12:18:55 +0530

    Create concurrentPgm_NoComm.cpp

---
+++ Phase2/Day35/concurrentPgm_NoComm.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<unistd.h>
+
+void client() { //child 1
+    sleep(1);
+    std::cout << "client" << std::endl;
+}
+
+void server() { //child 2
+    sleep(1);
+    std::cout << "server" << std::endl;
+}
+
+int main() {
+    pid_t pid = -1;
+
+    {   //child 1
+       pid_t pid = fork();
+        if(0 == pid) {
+            server();
+            return 0;
+        }
+    }
+
+    {   //child 2
+       pid_t pid = fork();
+        if(0 == pid) {
+            client();
+            return 0;
+        }
+    }
+
+    // no parent process
+    sleep(2);
+    return 0;
+}
