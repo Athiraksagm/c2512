ef3028fb3619e9342ecde9b0ef0b2b269d1045b5
Athira K S <athiraksagm@gmail.com>
2025-02-16 21:11:26 +0530

    Create client.cpp

---
+++ SystemStatLoggingMicroservice/source/client.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <httplib.h>
+#include <json.hpp>
+#include <thread>
+#include <chrono>
+
+using json = nlohmann::json;
+
+void requestStats() 
+{
+    httplib::Client client("http://localhost:8080");
+
+    while (true) {
+        auto res = client.Get("/stats");
+
+        if (res && res->status == 200) {
+            json stats = json::parse(res->body);
+
+            std::cout << "Received stats:\n";
+            std::cout << "Boot Count: " << stats["boot_count"] << "\n";
+            std::cout << "Uptime: " << stats["uptime"] << " seconds\n";
+        } else {
+            std::cerr << "Failed to get stats from server.\n";
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+}
+
+int main() 
+{
+    std::cout << "Client is requesting stats from server...\n";
+    requestStats();
+    return 0;
+}
